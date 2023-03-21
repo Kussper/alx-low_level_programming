c80e0869fc5dc4dae9f3d044f622954aa08532bb
kussper <kusspermahmoud@gmail.com>
2023-03-21 12:46:40 -0700

    kussper

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
 #include "main.h"
 	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
+	 * main - Entry point
+	 * 
+	 * Description: prints _putchar using putchar prototype
+	 *
+	 * Return: Always 0 (Success)
 	 */
 
 
